4cdb9c347e6d5b8eb62cf32df91f9f2018ba378b
TianMi <rccq17mj@live.cn>
2018-10-24 09:20:34 +0800

    结束工程shell

---
+++ shell.js
@@ -0,0 +1,20 @@
+const process = require('child_process');
+const os = require('os');
+
+// mac平台，停止所有的服务
+if(os.EOL == '\n') {
+  process.exec('kill -9 $(ps -ef|grep node |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+
+
+  process.exec('kill -9 $(ps -ef|grep ng |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+
+  console.log('ng sever: kill');
+}
