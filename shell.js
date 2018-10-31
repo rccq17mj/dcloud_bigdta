0980fb10036a7e75b3791cef0a3cc23a3e0d46a8
TianMi <rccq17mj@live.cn>
2018-10-31 16:04:30 +0800

    add

--- shell.js
+++ shell.js
@@ -3,14 +3,14 @@ const os = require('os');
 
 // mac平台，停止所有的服务
 if(os.EOL == '\n') {
-  process.exec('kill -9 $(ps -ef|grep node |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
+  process.exec('kill -9 $(ps -ef|lsof -i tcp:9100 |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
     }
   });
 
 
-  process.exec('kill -9 $(ps -ef|grep ng |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
+  process.exec('kill -9 $(ps -ef|lsof -i tcp:9102 |awk \'$0 !~/grep/ {print $2}\' |tr -s \'\\n\' \' \')', function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
     }
