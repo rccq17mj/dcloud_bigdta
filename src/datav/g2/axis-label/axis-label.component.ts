a0e8cea1724b4e3a084c457878cf1fab227c8017
TianMi <rccq17mj@live.cn>
2018-11-08 17:31:52 +0800

    组件

--- src/datav/g2/axis-label/axis-label.component.ts
+++ src/datav/g2/axis-label/axis-label.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import * as G2 from '@antv/g2';
 
 @Component({
@@ -8,33 +8,56 @@ import * as G2 from '@antv/g2';
 })
 export class AxisLabelComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  dataConfig?: object;
 
-  title = 'app';
-  data = {};
-  chart ;
-  graph;
+  /**
+   * 渲染回调，组件会把真实的configure回传回pages
+   */
+  @Output()
+  call_back? = new EventEmitter<object>();
 
-  ngOnInit() {
-    this.chartData();
-  }
-  chartData() {
-    this.data = [
+  /**
+   * 当前组件有哪些可维护的属性
+   * @type {{data: {genre: string; sold: number}[]; chart: {width: number; height: number}}}
+   */
+  configure = {
+    data: [
       { genre: 'Sports', sold: 275 },
       { genre: 'Strategy', sold: 115 },
       { genre: 'Action', sold: 120 },
       { genre: 'Shooter', sold: 350 },
       { genre: 'Other', sold: 150 }
-    ];
-    this.chart = new G2.Chart({
-      container: 'axis_label', // 指定图表容器 ID
+    ],
+    chart: {
       width : 600, // 指定图表宽度
       height : 300 // 指定图表高度
-    });
+    }
+  };
 
-    this.chart.source(this.data);
+  // g2chart
+  chart ;
+
+  /**
+   * 用于外部配置
+   * @param {object} dataConfig
+   */
+  constructor() {}
+
+  ngOnInit() {
+    if(this.dataConfig)
+      this.configure = {...this.configure,...this.dataConfig};
+    this.chartData();
+  }
+
+  chartData() {
+    this.chart = new G2.Chart({...this.configure.chart,...{container: 'axis_label'}});
+
+    this.chart.source(this.configure.data);
     this.chart.interval().position('genre*sold').color('genre');
     //  渲染图表
     this.chart.render();
+
+    this.call_back.emit(this.configure);
   }
 }
